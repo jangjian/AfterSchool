5d8a2aa59ee55e32967096c7e6b686d6364fb61a
jangjian <pureunnabi@gmail.com>
2022-12-26 11:37:05 +0900

    SFML 기본을 완성

--- Afterschool/main.cpp
+++ Afterschool/main.cpp
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
 
 int main(void) {
-	printf("hello world");
+	//윈도우창 생성
+	RenderWindow window(VideoMode(640, 480), "AfterSchool");
+
+	while (window.isOpen()) {
+		Event event;
+		while (window.pollEvent(event)) {
+			if(event.type == Event::Closed)
+				window.close();
+		}
+			
+	}
 
 	return 0;
 }
