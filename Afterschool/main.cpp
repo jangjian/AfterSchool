fa200b28114dabaf46977cd7050593e892177cec
jangjian <pureunnabi@gmail.com>
2022-12-26 13:53:47 +0900

    네모그리기

--- Afterschool/main.cpp
+++ Afterschool/main.cpp
@@ -7,14 +7,24 @@ int main(void) {
 	//윈도우창 생성
 	RenderWindow window(VideoMode(640, 480), "AfterSchool");
 
+	RectangleShape player;
+	player.setSize(Vector2f(40, 40));
+	player.setPosition(100,100);
+	
+
 	while (window.isOpen()) {
 		Event event;
 		while (window.pollEvent(event)) {
-			if(event.type == Event::Closed)
-				window.close();
-		}
-			
-	}
+			switch (event.type) {
 
+				//종료(x)버튼을 누르면 Event::Closed(0)
+			case Event::Closed:
+				window.close();
+			}
+
+		}
+		window.draw(player);
+		window.display();
+	}
 	return 0;
 }
--- Project1/Project1/switch.c
+++ Project1/Project1/switch.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 
-int main(void) {
-	int a = 1;
-
-	if (a == 1) {
-		printf("apple");
-	}else if
-		printf("banana");
-	else 
-		printf("mango");
-
-	return 0;
+void main(void) {
+	int a, b, c = 0;
+	a = ++c;
+	b = c++;
+	printf("%d %d %d\n", a, b, ++c);
 }
