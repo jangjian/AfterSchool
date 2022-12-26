b2ba03a054193ca4a7320240838f6582ffdc0ee4
jangjian <pureunnabi@gmail.com>
2022-12-26 14:04:58 +0900

    플레이어 색깔 변경

--- Afterschool/main.cpp
+++ Afterschool/main.cpp
@@ -10,7 +10,7 @@ int main(void) {
 	RectangleShape player;
 	player.setSize(Vector2f(40, 40));
 	player.setPosition(100,100);
-	
+	player.setFillColor(Color::Red);
 
 	while (window.isOpen()) {
 		Event event;
