8c6de75ffc030edd1999c8f35ccdeedbd32633c3
jangjian <pureunnabi@gmail.com>
2022-12-26 14:35:25 +0900

    적 만들기

--- Afterschool/main.cpp
+++ Afterschool/main.cpp
@@ -1,30 +1,54 @@
 #include <stdio.h>
 #include <SFML/Graphics.hpp>
-
 using namespace sf;
+int main(void)
+{
+    //윈도우창 생성
+    RenderWindow window(VideoMode(640, 480), "AfterSchool");
+    window.setFramerateLimit(60);
+    RectangleShape player;
+    player.setSize(Vector2f(40, 40));
+    player.setPosition(100, 100);
+    player.setFillColor(Color::Red);
+    int player_speed = 5;
 
-int main(void) {
-	//윈도우창 생성
-	RenderWindow window(VideoMode(640, 480), "AfterSchool");
+    RectangleShape enemy;
+    enemy.setSize(Vector2f(70, 70));
+    enemy.setPosition(500, 300);
+    enemy.setFillColor(Color::Yellow);
 
-	RectangleShape player;
-	player.setSize(Vector2f(40, 40));
-	player.setPosition(100,100);
-	player.setFillColor(Color::Red);
 
-	while (window.isOpen()) {
-		Event event;
-		while (window.pollEvent(event)) {
-			switch (event.type) {
+    //윈도우가 열려있을 때까지 반복
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event)) {
+            //종료(X) 버튼을 누르면
+            switch (event.type)
+            {
+            case Event::Closed:
+                window.close();//윈도우 닫는다.
+            }
 
-				//종료(x)버튼을 누르면 Event::Closed(0)
-			case Event::Closed:
-				window.close();
-			}
 
-		}
-		window.draw(player);
-		window.display();
-	}
-	return 0;
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Left)) {
+            player.move(-player_speed, 0);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Right)) {
+            player.move(player_speed, 0);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Up)) {
+            player.move(0, -player_speed);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Down)) {
+            player.move(0, player_speed);
+        }
+        window.clear(Color::Black);
+        window.draw(enemy);
+        window.draw(player);
+        window.display();
+    }
+
+    return 0;
 }
