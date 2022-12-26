285b9f115f8d28e85a57d424cb9ffed50f4b2ab3
jangjian <pureunnabi@gmail.com>
2022-12-26 15:05:36 +0900

    몬스터 제거 기능 추가

--- Afterschool/main.cpp
+++ Afterschool/main.cpp
@@ -16,6 +16,7 @@ int main(void)
     enemy.setSize(Vector2f(70, 70));
     enemy.setPosition(500, 300);
     enemy.setFillColor(Color::Yellow);
+    int enemy_life = 1;
 
 
     //윈도우가 열려있을 때까지 반복
@@ -45,14 +46,23 @@ int main(void)
             player.move(0, player_speed);
         }
 
-        if (player.getGlobalBounds().intersects(enemy.getGlobalBounds()))
+        //enemy와의 충돌
+        if(enemy_life > 0)
         {
-            printf("enemy과 충돌\n");
+            if (player.getGlobalBounds().intersects(enemy.getGlobalBounds()))
+            {
+                printf("enemy과 충돌\n");
+                enemy_life -= 1;
+            }
+            
         }
+
         window.clear(Color::Black);
 
         //draw는 나중에 호출할수록 우선 순위가 높아짐
-        window.draw(enemy);
+        if (enemy_life > 0) 
+            window.draw(enemy);
+        
         window.draw(player);
         window.display();
     }
