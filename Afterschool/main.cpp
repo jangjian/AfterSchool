0ad262fd045ead7b2574aa167fedd1d3e5a1ccdb
jangjian <pureunnabi@gmail.com>
2022-12-27 13:24:39 +0900

    사운드 추가 및 타이머 추가

--- Afterschool/main.cpp
+++ Afterschool/main.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <SFML/Graphics.hpp>
 #include <time.h>
+#include <SFML/Audio.hpp>
 
 using namespace sf;
 
@@ -15,17 +16,21 @@ int main(void)
 
     srand(time(0));
 
+    long start_time = clock(); //게임 시작 시간
+    long spent_time; //게임 진행 시간
+
+    //text
     Font font;
     font.loadFromFile("C:\\Windows\\Fonts\\arial.ttf");
     
     Text text;
     text.setFont(font);
-    text.setCharacterSize(50);  //폰트 크기
+    text.setCharacterSize(30);  //폰트 크기
     text.setFillColor(Color(255, 255, 255));
     text.setPosition(0, 0);
     char info[40];
     
-
+    // 플레이어
     RectangleShape player;
     player.setSize(Vector2f(40, 40));
     player.setPosition(100, 100);
@@ -33,11 +38,14 @@ int main(void)
     int player_speed = 5;
     int player_score = 0;
     
-    
-
+    //적
     RectangleShape enemy[5];
     int enemy_life[5];
     int enemy_score = 100;  //적을 잡을 때 얻는 점수
+    SoundBuffer enemy_explosion_buffer;
+    enemy_explosion_buffer.loadFromFile("./resources/sounds/rumble.flac");
+    Sound enemy_explosion_sound;
+    enemy_explosion_sound.setBuffer(enemy_explosion_buffer);
     //enemy 초기화
     for (int i = 0; i < 5; i++) {
         enemy[i].setSize(Vector2f(70, 70));
@@ -76,6 +84,8 @@ int main(void)
             }
         }
 
+        spent_time = clock() - start_time;
+
         //방향키 start
         if (Keyboard::isKeyPressed(Keyboard::Left)) {
             player.move(-player_speed, 0);
@@ -101,12 +111,20 @@ int main(void)
                     printf("enemy[%d]과 충돌\n", i);
                     enemy_life[i] -= 1;
                     player_score += enemy_score;
+
+                    //TODO : 코드 refactoring 필요
+                    if (enemy_life[i] == 0)
+                    {
+                        enemy_explosion_sound.play();
+                    }
                 }
 
             }
+   
         }
 
-        sprintf(info, "score : %d\n", player_score);
+        sprintf(info, "score : %d time : %d"
+            , player_score, spent_time/1000);
         text.setString(info);
 
         window.clear(Color::Black);
