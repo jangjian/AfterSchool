5957ba6b17e89073bd0eef59d81aa49c94855214
jangjian <pureunnabi@gmail.com>
2022-12-30 15:31:27 +0900

    똑같은 카드 2개씩 배치

--- AfterSchool2/main.cpp
+++ AfterSchool2/main.cpp
@@ -6,6 +6,17 @@
 
 using namespace sf;
 
+const int S = 4;
+const int CARD_W = 150;
+const int CARD_H = 200;
+
+struct Card
+{
+	RectangleShape sprite;
+	int id;
+	int type;
+};
+
 int main(void)
 {
 	RenderWindow window(VideoMode(1200, 800), "AfterSchool2");
@@ -13,6 +24,18 @@ int main(void)
 
 	Vector2i mouse_pos;
 	int click_cnt = 0;		//마우스 누른 횟수
+
+	Texture t[8 + 1];
+	t[0].loadFromFile("./resources/images/card8.png");
+	t[1].loadFromFile("./resources/images/card1.png");
+	t[2].loadFromFile("./resources/images/card2.png");
+	t[3].loadFromFile("./resources/images/card3.png");
+	t[4].loadFromFile("./resources/images/card4.png");
+	t[5].loadFromFile("./resources/images/card5.png");
+	t[6].loadFromFile("./resources/images/card6.png");
+	t[7].loadFromFile("./resources/images/card7.png");
+	t[8].loadFromFile("./resources/images/card0.png");
+
 
 	Font font;
 	font.loadFromFile("C:\\Windows\\Fonts\\arial.ttf");
@@ -23,6 +46,21 @@ int main(void)
 	text.setFillColor(Color::White);
 	text.setPosition(0, 0);
 	char info[40];
+
+	struct Card cards[S][S];
+	int n = 0;
+	for (int i = 0; i < S; i++)
+	{
+		for (int j = 0; j < S; j++)
+		{
+			cards[i][j].sprite.setSize(Vector2f(CARD_W, CARD_H));
+			cards[i][j].sprite.setPosition(j * CARD_W, i * CARD_H);
+			cards[i][j].sprite.setTexture(&t[1 + n / 2]);
+			cards[i][j].type = 1 + n / 2;
+			cards[i][j].id = n + 1;
+			n++;
+		}
+	}
 
 	while (window.isOpen())
 	{
@@ -49,6 +87,13 @@ int main(void)
 		text.setString(info);
 
 		window.clear(Color::Black);
+		for (int i = 0; i < S; i++)
+		{
+			for (int j = 0; j < S; j++)
+			{
+				window.draw(cards[i][j].sprite);
+			}
+		}
 		window.draw(text);
 		window.display();
 	}
