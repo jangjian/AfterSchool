9b36ed10685b2dba7902686ddbbc6924b879f625
jangjian <pureunnabi@gmail.com>
2023-01-03 15:12:53 +0900

    밸런스 조절

--- Moonbub1/Animation.cpp
+++ Moonbub1/Animation.cpp
@@ -1,11 +1,15 @@
 #include <SFML/Graphics.hpp>
+#include <time.h>
 
 using namespace sf;
 
 struct Player {
 	RectangleShape sprite;
-	int fps;			// 
-	int idx;			// 애니메이션 index
+	int fps;					// frames per sec
+	int idx;					// 애니메이션 index
+	int frames;					// 애니메이션 frame수
+	long ani_time;				// 애니메이션이 바뀔 때의 시각
+	long ani_delay;
 };
 
 int main(void)
@@ -13,26 +17,36 @@ int main(void)
 	RenderWindow window(VideoMode(1200, 800), "Animation");
 	window.setFramerateLimit(60);
 
+	long start_time;
+	long spent_time;
+
 	Texture run[10];
-	run[0].loadFromFile("./animation/run__000.png");
-	run[1].loadFromFile("./animation/run__001.png");
-	run[2].loadFromFile("./animation/run__002.png");
-	run[3].loadFromFile("./animation/run__003.png");
-	run[4].loadFromFile("./animation/run__004.png");
-	run[5].loadFromFile("./animation/run__005.png");
-	run[6].loadFromFile("./animation/run__006.png");
-	run[7].loadFromFile("./animation/run__007.png");
-	run[8].loadFromFile("./animation/run__008.png");
-	run[9].loadFromFile("./animation/run__009.png");
+	run[0].loadFromFile("./animation/Run__000.png");
+	run[1].loadFromFile("./animation/Run__001.png");
+	run[2].loadFromFile("./animation/Run__002.png");
+	run[3].loadFromFile("./animation/Run__003.png");
+	run[4].loadFromFile("./animation/Run__004.png");
+	run[5].loadFromFile("./animation/Run__005.png");
+	run[6].loadFromFile("./animation/Run__006.png");
+	run[7].loadFromFile("./animation/Run__007.png");
+	run[8].loadFromFile("./animation/Run__008.png");
+	run[9].loadFromFile("./animation/Run__009.png");
 
 	struct Player player;
 	player.fps = 10;
+	player.frames = 10;
+	player.idx = 0;
 	player.sprite.setTexture(&run[0]);
 	player.sprite.setSize(Vector2f(90, 120));
-	player.sprite.setPosition(200, 600);
-	
+	player.sprite.setPosition(200, 400);
+	player.ani_delay = 1000/ player.frames / 2;		//0.5초마다 걸음
+
+	start_time = clock();
+	player.ani_time = start_time;
+
 	while (window.isOpen())
 	{
+		spent_time = clock() - start_time;
 		Event event;
 		while (window.pollEvent(event))
 		{
@@ -45,10 +59,21 @@ int main(void)
 				break;
 			}
 		}
+
+		// 0.1초마다 애니메이션 그림이 바뀜
+		while (spent_time - player.ani_time > 1000 / player.ani_delay)
+		{
+			player.ani_time = spent_time;
+			player.sprite.setTexture(&run[player.idx % player.frames]);
+			player.idx++;
+		}
+
 		window.clear(Color::Magenta);
 
 		window.draw(player.sprite);
+
 		window.display();
 	}
+
 	return 0;
 }
